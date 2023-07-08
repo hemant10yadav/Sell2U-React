3fb3589bcd6c6413bd7abd947b31f58cb0bbf0b2
Hemant Singh Yadav <98798073+hemant10yadav@users.noreply.github.com>
2023-07-08 11:02:46 +0530

    changed-server-and-client-repos

--- backend/src/util/paths.ts
+++
@@ -1,6 +0,0 @@
-export default class Paths {
-   public static AUTH = '/auth';
-   public static SIGNUP = '/signup';
-   public static LOGIN = '/login';
-   public static RESET_PASSWORD = '/reset-password';
-}
